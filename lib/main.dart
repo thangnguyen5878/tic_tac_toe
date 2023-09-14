da56f9e9856953953db7e6920dec8741038ff73a
thang5878 <thang5878@gmail.com>
2023-09-14 09:12:34 +0700

    basic: firebase, google sign in button

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tic_tac_toe/views/pages/input_page.dart';
+import 'package:tic_tac_toe/views/pages/signin_page.dart';
 import 'package:tic_tac_toe/views/pages/winner_page.dart';
 
 import 'controllers/global_controller.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -29,7 +32,7 @@ class MyApp extends StatelessWidget {
           // transition: Transition.downToUp
         )
       ],
-      home: InputPage(),
+      home: SigninPage(),
     );
   }
 }
